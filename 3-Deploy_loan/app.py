7f4532101cef33b855aacf10487ea7bd836af3ad
Pedro Lopes <109923335+PLeonLopes@users.noreply.github.com>
2024-11-17 01:15:03 -0300

    Update app.py
    PORTS

--- 3-Deploy_loan/app.py
+++ 3-Deploy_loan/app.py
@@ -97,5 +97,6 @@ def prever():
         return render_template("result.html", client_name="Erro. Tente novamente", mensagem=str(e), cor="danger")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
